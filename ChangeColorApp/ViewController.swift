faba4d603688a78d687a99bb32a821d88bf59b80
maximsamus <maximsamus1@gmail.com>
2022-03-05 15:25:42 +0400

    UI

--- ChangeColorApp/ViewController.swift
+++ ChangeColorApp/ViewController.swift
@@ -8,11 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var mainView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        mainView.layer.cornerRadius = 10
+        
+        }
 
 
 }
