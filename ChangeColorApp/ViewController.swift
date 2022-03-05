ac1c7d96cd5fa930ccbeaa89b8e80071073c3e91
maximsamus <maximsamus1@gmail.com>
2022-03-05 21:42:57 +0400

    Logic setup

--- ChangeColorApp/ViewController.swift
+++ ChangeColorApp/ViewController.swift
@@ -14,14 +14,37 @@ class ViewController: UIViewController {
     @IBOutlet var green: UILabel!
     @IBOutlet var blue: UILabel!
     
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         mainView.layer.cornerRadius = 10
-        red.text = "Red"
-        green.text = "Green"
-        blue.text = "Blue"
+        red.text = "Red:"
+        green.text = "Green:"
+        blue.text = "Blue:"
+        red.textColor = .white
+        green.textColor = .white
+        blue.textColor = .white
+        
+        redSlider.value = 0
+        redSlider.maximumValue = 1
+        redSlider.minimumValue = 0
+        redSlider.tintColor = .red
+        
+        greenSlider.value = 0
+        greenSlider.maximumValue = 1
+        greenSlider.minimumValue = 0
+        greenSlider.tintColor = .green
+        
+        blueSlider.value = 0
+        blueSlider.maximumValue = 1
+        blueSlider.minimumValue = 0
+        blueSlider.tintColor = .blue
         }
-
+    
 
 }
 
