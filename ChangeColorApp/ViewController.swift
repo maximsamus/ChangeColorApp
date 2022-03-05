99ee2b3a80fadc7dd57708f01ac35626078d36c3
maximsamus <maximsamus1@gmail.com>
2022-03-05 20:04:55 +0400

    UI Update

--- ChangeColorApp/ViewController.swift
+++ ChangeColorApp/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var mainView: UIView!
+    @IBOutlet var red: UILabel!
+    @IBOutlet var green: UILabel!
+    @IBOutlet var blue: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mainView.layer.cornerRadius = 10
-        
+        red.text = "Red"
+        green.text = "Green"
+        blue.text = "Blue"
         }
 
 
